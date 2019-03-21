b654e92a7b067fe4f66bc2f26bdfcf16c34f1a92
Aleksey <alekseyka2000@gmail.com>
2019-03-21 16:29:08 +0300

    Initial commit
    Signed-off-by: Aleksey <alekseyka2000@gmail.com>

--- app/src/main/java/com/startandroid/myapplication/MainActivity.java
+++ app/src/main/java/com/startandroid/myapplication/MainActivity.java
@@ -4,18 +4,22 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.IntentFilter;
 import android.net.wifi.WifiManager;
+import android.net.wifi.p2p.WifiP2pConfig;
 import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pInfo;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     TextView tvData, tvStatusConnection;
     Button btnWifi, btnGetData, btnDiscover;
     ListView listView;
+    TextView tvMessage;
 
     WifiManager wifiManager;
     WifiP2pManager mManager;
@@ -32,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
     BroadcastReceiver mReceiver;
     IntentFilter intentFilter;
 
-    List<WifiP2pDevice> peers = new ArrayList<WifiP2pDevice>();
+    List<WifiP2pDevice> peers = new ArrayList<>();
     String[] deviceNameArray;
     WifiP2pDevice[] deviceArray;
 
@@ -61,13 +66,25 @@ public class MainActivity extends AppCompatActivity {
                     index++;
                 }
 
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, deviceNameArray);
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, deviceNameArray);
                 listView.setAdapter(adapter);
             }
 
             if (peers.size() == 0) {
                 Toast.makeText(getApplicationContext(), "No Device Found", Toast.LENGTH_SHORT).show();
                 return;
+            }
+        }
+    };
+    WifiP2pManager.ConnectionInfoListener connectionInfoListener = new WifiP2pManager.ConnectionInfoListener() {
+        @Override
+        public void onConnectionInfoAvailable(WifiP2pInfo wifiP2pInfo) {
+            final InetAddress groupOwnerAddress = wifiP2pInfo.groupOwnerAddress;
+
+            if (wifiP2pInfo.groupFormed && wifiP2pInfo.isGroupOwner) {
+                tvStatusConnection.setText("Host");
+            } else if (wifiP2pInfo.groupFormed) {
+                tvStatusConnection.setText("Client");
             }
         }
     };
@@ -101,6 +118,28 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                final WifiP2pDevice device = deviceArray[i];
+                WifiP2pConfig config = new WifiP2pConfig();
+                config.deviceAddress = device.deviceAddress;
+
+                mManager.connect(mChannel, config, new WifiP2pManager.ActionListener() {
+                    @Override
+                    public void onSuccess() {
+                        Toast.makeText(getApplicationContext(), "Connected to " + device.deviceName, Toast.LENGTH_SHORT).show();
+                        String msg = tvMessage.getText().toString();
+                    }
+
+                    @Override
+                    public void onFailure(int i) {
+                        Toast.makeText(getApplicationContext(), "Not Connected", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
     }
 
     private void initialWork() {
@@ -116,14 +155,15 @@ public class MainActivity extends AppCompatActivity {
             wifiManager.setWifiEnabled(true);
             btnWifi.setText("WIFI OFF");
         }
+        tvMessage = findViewById(R.id.message);
         tvData = findViewById(R.id.data);
         tvStatusConnection = findViewById(R.id.statusConnection);
         listView = findViewById(R.id.peerListView);
 
         mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
-        mChannel = mManager.initialize(this,getMainLooper(),null);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
 
-        mReceiver = new WiFiDirectBroadcastReceiver(mManager,mChannel,this);
+        mReceiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, this);
 
         intentFilter = new IntentFilter();
         intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
@@ -135,7 +175,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        registerReceiver(mReceiver,intentFilter);
+        registerReceiver(mReceiver, intentFilter);
     }
 
     @Override
@@ -143,4 +183,5 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         unregisterReceiver(mReceiver);
     }
+
 }
